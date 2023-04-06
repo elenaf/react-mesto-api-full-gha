6f0cb4fc883744513e7bf78b77a1be2246bd3830
Elena Fedotova <e-a-fedotova@yandex.ru>
2023-04-07 01:02:33 +0300

    поменять фронт

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,32 @@
-export default function Header() {
+import React from "react";
+import { NavLink, Route, Switch } from 'react-router-dom';
+
+export default function Header(props) {
+  const email = props.userData;
+  const loggedIn = props.loggedIn;
+  const signOut = props.onSignOut;
+
     return(
     <header className="header">
       <div className="header__logo"></div>
+      <div className="header__user-data">
+        
+        <Switch>
+          <Route exact path="/">
+              <p className="header__user-email">{loggedIn && email}</p>
+              <NavLink to="/sign-out" onClick={signOut} className="header__link">Выйти</NavLink>
+          </Route>
+          <Route path="/sign-in">
+            <NavLink to="/sign-up" className="header__link">Зарегистрироваться</NavLink>
+          </Route>
+          <Route path="/sign-up">
+            <NavLink to="/sign-in" className="header__link">Войти</NavLink>
+          </Route>
+        </Switch>
+        
+      </div>
     </header>
     );
 }
+
 
