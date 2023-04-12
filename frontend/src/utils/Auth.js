b12f58dad6bffa019e4a6cc70876b98abc3938a7
Elena Fedotova <e-a-fedotova@yandex.ru>
2023-04-13 01:01:09 +0300

    base url поменять

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://mesto-react.nomoredomains.monster'
+export const BASE_URL = 'https://api.mesto-react.nomoredomains.monster'
 // export const BASE_URL = 'http://localhost:8000'
 // export const BASE_URL = 'https://auth.nomoreparties.co';
 const defaultHeaders = {
